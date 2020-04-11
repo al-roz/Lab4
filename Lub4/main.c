357c03bc4d70319db875f206dd1d4c94a29c9b77
Roz <sasha_royz@mail.ru>
2020-04-11 23:41:19 +0500

    add HandRead and output

--- Lub4/main.c
+++ Lub4/main.c
@@ -1,7 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+#define MAX 100
+
+struct entry
+{
+	int old;
+	char* name;
+	char* sernam;
+	char* grup;
+	char telephonNumber[12];
+	int	assessments[5];
+	int id;
+};
+typedef struct entry ENTRY;
+
+struct list
+{
+	ENTRY person;
+	struct list* next;
+};
+typedef struct list LIST;
+
+void add(LIST* head, ENTRY* addValue)
+{
+	LIST* tmp;
+	tmp = (LIST*)malloc(sizeof(LIST));
+
+	tmp->person.name = (unsigned char*)malloc(addValue->name);
+	strcpy_s(tmp->person.name, strlen(addValue->name) + 1, addValue->name);
+
+	tmp->person.sernam = (unsigned char*)malloc(addValue->sernam);
+	strcpy_s(tmp->person.sernam, strlen(addValue->sernam) + 1, addValue->sernam);
+
+	tmp->person.old = addValue->old;
+
+	tmp->person.grup = (unsigned char*)malloc(addValue->grup);
+	strcpy_s(tmp->person.grup, strlen(addValue->grup) + 1, addValue->grup);
+
+	strcpy_s(tmp->person.telephonNumber, 12, addValue->telephonNumber);
+
+	for (int i = 0; i < 5; i++)
+	{
+		tmp->person.assessments[i] = addValue->assessments[i];
+	}
+
+	tmp->next = 0;
+	LIST* curr = head;
+	while (curr->next != 0)
+		curr = curr->next;
+	curr->next = tmp;
+}
+
+void output(LIST* head)
+{
+	while (head)
+	{
+		if (head->person.id != 0) {
+			printf("%s %s %d %s %s\n", head->person.sernam , head->person.name, head->person.old, head->person.grup, head->person.telephonNumber);
+			for (int i = 0; i < 5; i++)
+			{
+				printf("%d ", head->person.assessments[i]);
+			}
+			printf("\n");
+		}
+		head = head->next;
+	}
+}
 
 int main()
 {
+	setlocale(LC_ALL, "Ru");
+	LIST* head = (LIST*)malloc(sizeof(LIST));
+	head->next = NULL;
+	head->person.id = 0;
+	ENTRY addValue;
+	int k = 1;
+	while (1)
+	{
+		printf("Введите данные для добавления(фамилия, имя, возраст, группа, телефон,(оценки - 5 штук), начиная с новой строки\n");
+		char s[256];
+		
+		addValue.id = k;
+		k++;
 
+		gets(s);
+		addValue.sernam = (char*)malloc(strlen(s) * sizeof(char));
+		strcpy_s(addValue.sernam,strlen(s)+1, s);
+
+		gets(s);
+		addValue.name = (char*)malloc(strlen(s) * sizeof(char));
+		strcpy_s(addValue.name, strlen(s) + 1, s);
+
+		scanf_s("%d", &addValue.old);
+		gets(s);
+
+		gets(s);
+		addValue.grup = (char*)malloc(strlen(s) * sizeof(char));
+		strcpy_s(addValue.grup, strlen(s) + 1, s);
+
+		gets(addValue.telephonNumber);
+		
+		for (int i = 0; i < 5; i++)
+			scanf_s("%d",&addValue.assessments[i]);
+			   		 
+		add(head, &addValue);
+		output(head);
+	}
+
+	
 }
