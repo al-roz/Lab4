7a6398c4846b8c3d34fe22149fa1fc39de0c31eb
Roz <sasha_royz@mail.ru>
2020-04-12 18:13:31 +0500

    end

--- Lub4/main.c
+++ Lub4/main.c
@@ -89,6 +89,116 @@ void output(LIST* head)
 	}
 }
 
+void outOtl(LIST* head)
+{
+	while (head)
+	{
+		if (head->person.id != 0) {
+			int flag = 1;
+			for (int i = 0; i < 5; i++)
+				if (head->person.assessments[i] < 4) flag = 0;
+			if (flag == 1)
+			{
+				printf("%s %s %d %s %s\n", head->person.sernam, head->person.name, head->person.old, head->person.grup, head->person.telephonNumber);
+				for (int i = 0; i < 5; i++)
+				{
+					printf("%d ", head->person.assessments[i]);
+				}
+				printf("\n");
+			}
+			
+		}
+		head = head->next;
+	}
+}
+
+void FilePrintList(LIST* head, char* FileName)
+{
+	FILE* f;
+	fopen_s(&f, FileName, "w");
+	while (head)
+	{
+		if (head->person.id != 0) {
+			fprintf(f,"%s\n %s\n %d\n %s\n %s\n ", head->person.sernam, head->person.name, head->person.old, head->person.grup, head->person.telephonNumber);
+			for (int i = 0; i < 5; i++)
+			{
+				fprintf(f,"%d ", head->person.assessments[i]);
+			}
+			fprintf(f,"\n");
+		}
+		head = head->next;
+	}
+	fclose(f);
+
+}
+
+void FileInputList(LIST* head, char* FileName)
+{
+	FILE* f;
+	fopen_s(&f, FileName, "r");
+	ENTRY addValue;
+	int k = 1;
+	char s[51];
+	while (!feof(f))
+	{
+
+		addValue.id = k;
+		k++;
+
+		fgets(s, 50, f);
+		s[strlen(s)-1] = 0;
+		addValue.sernam = (char*)malloc(strlen(s) * sizeof(char));
+		strcpy_s(addValue.sernam, strlen(s) + 1, s);
+
+		fgets(s, 50, f);
+		s[strlen(s)-1] = 0;
+		addValue.name = (char*)malloc(strlen(s) * sizeof(char));
+		strcpy_s(addValue.name, strlen(s) + 1, s);
+
+		fscanf_s(f, "%d", &addValue.old);
+		fgets(s, 50, f);
+
+		fgets(s, 50, f);
+		s[strlen(s)-1] = 0;
+		addValue.grup = (char*)malloc(strlen(s) * sizeof(char));
+		strcpy_s(addValue.grup, strlen(s) + 1, s);
+
+		fgets(addValue.telephonNumber, 50, f);
+		addValue.telephonNumber[strlen(addValue.telephonNumber)-1] = 0;
+		
+		char re;
+		for (int i = 0; i < 5; i++)
+		{
+			fscanf_s(f, "%d", &addValue.assessments[i]);
+			
+		}
+		fscanf_s(f,"%c", &re);
+		
+		add(head, &addValue);
+
+	}
+}
+
+void FindOnGrop(LIST* head, char* grup)
+{
+	while (head)
+	{
+		if (head->person.id != 0) {
+			
+			if (strcmp(head->person.grup, grup) == 0)
+			{
+				printf("%s %s %d %s %s\n", head->person.sernam, head->person.name, head->person.old, head->person.grup, head->person.telephonNumber);
+				for (int i = 0; i < 5; i++)
+				{
+					printf("%d ", head->person.assessments[i]);
+				}
+				printf("\n");
+			}
+		}
+		head = head->next;
+	}
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Ru");
@@ -104,6 +214,10 @@ int main()
 		printf("1.Добавить ученика в список\n");
 		printf("2.Удалить ученика из списка\n");
 		printf("3.Вывести учащихся из списка\n");
+		printf("4.Вывести успевающих студентов\n");
+		printf("5.Вывести учащихся в файл\n");
+		printf("6.Считать из файла\n");
+		printf("7.Вывести студентов из одной группы\n");
 
 		
 		scanf_s("%c", &key);
@@ -139,8 +253,7 @@ int main()
 				scanf_s("%d", &addValue.assessments[i]);
 			scanf_s("%c", &re);
 			add(head, &addValue);
-			scanf_s("%c", &re);
-			
+			system("pause");			
 			break;
 
 		case '2' :
@@ -149,13 +262,42 @@ int main()
 			gets(sername);
 			delete(head, sername);
 			break;
+
 		case '3' :
 			output(head);
+			system("pause");
 			break;
-		default:
-			break;
-		}
 
+		case '4' :
+			outOtl(head);
+			system("pause");
+			break;
+
+		case '5' :
+			printf("Введите имя файла\n");			
+			char FileName[30];
+			gets(FileName);
+			FilePrintList(head, FileName);
+			break;
+
+		case '6' :
+			printf("Введите имя файла\n");
+			FileName[30];
+			gets(FileName);
+			FileInputList(head, FileName);
+			break;
+
+		case '7' :
+			printf("Введите групу\n");
+			gets(s);
+			FindOnGrop(head, s);
+			system("pause");
+			break;
+
+		default:
+			exit(1);
+		}
+		system("cls");
 		
 		
 
